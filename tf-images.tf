45c6c18f0ad7e8645dae88c0d717911a02481188
Filip Stawicki <filip1.stawicki@coop.no>
2024-01-24 13:25:41 +0100

    test

--- tf-images.tf
+++ tf-images.tf
@@ -1,4 +1,4 @@
 locals {
-  my_app_staging_image = "app:latest"
-  my_app_prod_image = "app:latest"
+  my_app_staging_image = "nginx:1.20@sha256:a76df3b4f1478766631c794de7ff466aca466f995fd5bb216bb9643a3dd2a6bb"
+  my_app_prod_image = "nginx:1.20@sha256:a76df3b4f1478766631c794de7ff466aca466f995fd5bb216bb9643a3dd2a6bb"
 }
